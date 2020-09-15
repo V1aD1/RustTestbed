ffe89a50cc36bee6cec5dc8e5080b362e23c79c4
Vlad Baderca <v_baderca@wargaming.net>
2020-09-15 10:47:17 +0200

    Added some files

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let a = 1;
+    let b = 2.0;
+    let name = "Vlad";
+    let boolean = true;
+
+    println!("The value of a is: {}", a);
+    something()
+}
+
+fn something(){
+    println!("SOmehghint")
+}
